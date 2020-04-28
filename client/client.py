46588ebede68e5b19ea941fe037c974dd9e67541
Huzaifa Irfan <huzaifairfan2001@gmail.com>
2020-04-28 21:35:18 +0500

    game play and scoring

--- client/client.py
+++ client/client.py
@@ -14,7 +14,8 @@ sio = socketio.Client()
 username="aplayer"
 myid="0"
 
-
+players={}
+gameplay=[]
 
 
 @sio.event
@@ -31,10 +32,139 @@ def waiting():
     clearscreen()
     print("Waiting for second Player")
 
+# @sio.event
+# def sendplayers(getplayers):
+#     clearscreen()
+#     print("Starting the game")
+#     global players
+#     players=getplayers
+
+
+# @sio.event
+# def startinggame(num):
+#     clearscreen()
+#     print("Starting game in",num,"seconds")
+
+
+
+def drawscreen():
+    global gameplay
+    global players
+    print(players["1"]["name"]," : ",players["1"]["score"],"                 ",players["2"]["name"]," : ",players["2"]["score"])
+    for row in gameplay:
+        for item in row:
+            if item==1:
+                print("O",end="")
+            else:
+                print("-",end="")
+        print("")
+
+
+def asknum(whatval):
+	anum=None
+	while(anum==None):
+		try:
+			anum=int(input(whatval))
+		except:
+			print("Number required")
+			continue
+
+		# if (anum <1 or anum >9):
+		# 	print("Write a Number (from 1-9)")
+		# 	anum=None
+
+
+	return anum
+
+
+
+def checkrow(rownum):
+    if rownum<1 or rownum>3:
+        return False
+    nitems=0
+    rowitems=gameplay[rownum-1]
+    for item in rowitems:
+        if item==1:
+            nitems=nitems+1
+    if nitems==0:
+        print("No items in this row please select another")
+        return False
+    return True
+        
+
+
+def askrow():
+    rownum=0
+    while(not checkrow(rownum)):
+        rownum=asknum("Enter Row Number 1-3")
+    asknumbers(rownum)
+
+
+def checkitems(rownum,itemnum):
+    if itemnum==0:
+        return False
+    nitems=0
+    rowitems=gameplay[rownum-1]
+    for item in rowitems:
+        if item==1:
+            nitems=nitems+1
+    if itemnum<=nitems:
+        return True
+    else:
+        print("Too Many")
+        return False
+
+
+def removeitems(rownum,itemnum):
+    global gameplay
+    removed=0
+    temprow=gameplay[rownum-1]
+    length=len(temprow)
+    for i in range(0,length):
+        if removed < itemnum:
+            if temprow[i]==1:
+                temprow[i]=0
+                removed= removed+1
+    gameplay[rownum-1]=temprow
+    #print(gameplay)
+    input("")
+    sio.emit('sendgameplay', gameplay)
+
+
+
+def asknumbers(rownum):
+    itemnum=0
+    while(not checkitems(rownum,itemnum)):
+          itemnum=asknum("Enter Number of pearls 1-3,4,5")
+    removeitems(rownum,itemnum)
+
+
+
+
+
 @sio.event
-def sendplayers():
+def sendgame(getgame):
     clearscreen()
-    print("Starting the game")
+    global players
+    global gameplay
+    players=getgame["players"]
+    gameplay=getgame["gameplay"]
+    # print(players)
+    if players[myid]["turn"]==True:
+        print("Your Turn")
+    else:
+        print("Opponent's Turn")
+    drawscreen()
+    if players[myid]["turn"]==True:
+        askrow()
+    
+
+
+@sio.event
+def playersfull():
+    clearscreen()
+    print("Two Players are already playing")
+
 
 @sio.event
 def disconnect():
--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@
 port=5000
 
 print("Pearl Before Swine Multiplayer Clone Server")
-
+import time
 
 
 from flask import Flask, render_template, session, request,jsonify, send_from_directory, \
@@ -28,7 +28,7 @@ socketio = SocketIO(app, async_mode=async_mode)
 
 
 
-
+ids=["1","2"]
 
 players={"1":None,"2":None}
 
@@ -37,8 +37,11 @@ gameplay=[[1,1,1,1,1],[1,1,1,1],[1,1,1]]
 
 
 def startgame():
-    emit("sendplayers",players,broadcast=True)
-    emit("sendgame",gameplay,broadcast=True)
+    #emit("sendplayers",players,broadcast=True)
+    # time.sleep(1)
+    global gameplay
+    global players
+    emit("sendgame",{"players":players,"gameplay":gameplay},broadcast=True)
 
 
 
@@ -47,7 +50,7 @@ def startgame():
 @socketio.on('Connection')
 def Connection(username):
     global players
-
+    print(request.sid)
     if players["1"]==None:
         players["1"]={"name":username,"turn":True,"score":0}
         emit("waiting")
@@ -63,6 +66,42 @@ def Connection(username):
             startgame()
         else:
             print("No space Left")
+            emit("playersfull")
+
+
+
+
+
+@socketio.on('sendgameplay')
+def sendgameplay(gotgameplay):
+    global gameplay
+    global players
+    global ids
+    gameplay=gotgameplay
+
+    left=0
+    for row in gameplay:
+        for item in row:
+            if item==1:
+                left=left+1
+
+    if left<2:
+        for aid in ids:
+            if players[aid]["turn"]==True:
+                gameplay=[[1,1,1,1,1],[1,1,1,1],[1,1,1]]
+                players[aid]["score"]=players[aid]["score"]+1
+
+
+    
+    players["1"]["turn"]=not players["1"]["turn"]
+    players["2"]["turn"]=not players["2"]["turn"]
+
+    emit("sendgame",{"players":players,"gameplay":gameplay},broadcast=True)
+
+
+
+
+
 
 
 
