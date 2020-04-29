6abd6f47d13b6fc851093ce1e8dce388e18810a7
Huzaifa Irfan <huzaifairfan2001@gmail.com>
2020-04-29 20:29:22 +0500

    create join and draw game

--- server/client/client.py
+++ server/client/client.py
@@ -1,17 +1,94 @@
 
 from os import system
-
+import time
 
 def clearscreen():
     system("cls")
     print("Pearl before Swine MultiPlayer Clone - Python TUI Client")
+
+def exitter(msg):
+    print(msg)
+    input("Return")
+
+
+
 
 
 import socketio
 
 sio = socketio.Client()
 
-username=input("Enter Your Name??\n")
+# username=input("Enter Your Name??\n")
+username="player"
+
+
+            
+
+
+
+def menu():
+    exit=False
+
+
+
+    # time.sleep(1)
+
+
+
+    while(not exit):
+        clearscreen()
+        print("C. Create Game")
+        print("F. Fetch Game")
+        print("X. Exit Game")
+        ch=input("Enter Number to Start")
+        if ch=="c"or ch=="C":
+            exitter("Creating Game")
+            sio.emit('creategame')
+            exit=True
+        elif ch=="f" or ch=="F":
+            exitter("Fetching Games")
+            sio.emit('fetchgames')
+            exit=True
+        elif ch=="x"or ch=="X":
+            sio.disconnect()
+            exit=True
+            exitter("Bye Bye")
+        else:
+            exitter("Please Choose from Above")
+            
+
+
+def asknum(msg,high):
+	anum=None
+	while(anum==None):
+		try:
+			anum=int(input(msg))
+		except:
+			print(f"Number required (from 1-{high})")
+			continue
+
+		if (anum <1 or anum >high):
+			print(f"Write a Number (from 1-{high})")
+			anum=None
+
+
+	return anum
+
+
+
+def drawgame(gamecontent):
+    clearscreen()
+    for row in gamecontent.values():
+        for item in row:
+            if item ==1:
+                print("O",end="")
+            else:
+                print("-",end="")
+        print("")
+
+
+
+
 
 
 
@@ -19,15 +96,61 @@ username=input("Enter Your Name??\n")
 
 @sio.event
 def connect():
-    print('connection established')
+    print(sio.sid)
+    print('Connection established')
+    menu()
+
+
+
+@sio.event
+def gamecreated():
+    clearscreen()
+    print("Game created")
+    print("Waiting for Player to Join")
+
+
+
+
+@sio.event
+def notfree(player1name):
+    clearscreen()
+    exitter(f"{player1name} Not Free")
+    menu()
+
+
+@sio.event
+def loadinggame(loader):
+    clearscreen()
+    print("Starting Game with",loader["opponent"])
+    drawgame(loader["game"]["gameplay"])
 
 
 
 # @sio.event
-# def sendid(gotid):
+# def startgame(game):
+#     clearscreen()
+#     exitter("Starting Game")
 
 
 
+@sio.event
+def showgames(freegames):
+    clearscreen()
+    print("Free Games")
+    print(freegames)
+    if len(freegames)>0:
+
+        for i in range(0,len(freegames)):
+            print(i+1,":",freegames[i]["creator"])
+        
+        gamenum=asknum("Enter Game ID",len(freegames))
+
+        sio.emit('joingame', freegames[gamenum-1]["gameid"])
+
+
+
+    else:
+        menu()
 
 
 
@@ -42,16 +165,17 @@ def disconnect():
 
 
 
-
-
-
 clearscreen()
 
 sio.connect('http://localhost:5000/')
 
-
-
 sio.emit('Connection', username)
+
+
+
+
+
 
 sio.wait()
 
+exitter("Exiting the Game")
--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ port=5000
 
 print("Pearl Before Swine Multiplayer Clone Server")
 import time
+import random
 
 
 from flask import Flask, render_template, session, request,jsonify, send_from_directory, \
@@ -33,13 +34,79 @@ games={}
 # gameplay=[[1,1,1,1,1],[1,1,1,1],[1,1,1]]
 
 
+
+
+
 @socketio.on('Connection')
 def Connection(username):
     global users
-    userobj={"username":username,"connected":True}
+    userobj={"userid":request.sid,"username":username,"connected":True,"opponent":None,"gameid":None}
     users[request.sid]=userobj
-    # print(users)
+    print(users)
     # emit("sendgame",{"players":players,"gameplay":gameplay},broadcast=True)
+
+
+
+@socketio.on('creategame')
+def creategame():
+    global users
+    global games
+    gameid=str(random.getrandbits(128))
+
+    users[request.sid]["gameid"]=gameid
+
+    gameobj={"gameid":gameid,"creator":users[request.sid]["username"],"player1":request.sid,"p1again":None,"player2":None,"p2again":None,"game":None}
+    games[gameid]=gameobj
+    # print(users)
+    emit("gamecreated")
+
+
+
+
+@socketio.on('fetchgames')
+def fetchgames():
+    global games
+    freegames=[]
+    for game in games.values():
+        if game["player2"]==None:
+            freegames.append(game)
+
+    emit("showgames",freegames)
+
+
+
+
+@socketio.on('joingame')
+def joingame(gotgameid):
+    global games
+    global users
+
+    if games[gotgameid]["player2"]==None:
+        games[gotgameid]["player2"]=request.sid
+        users[request.sid]["gameid"]=gotgameid
+        users[request.sid]["opponent"]=games[gotgameid]["player1"]
+        users[games[gotgameid]["player1"]]["opponent"]=games[gotgameid]["player2"]
+
+
+        games[gotgameid]["game"]={"gameid":gotgameid,"player1":{"username":users[games[gotgameid]["player1"]]["username"],"score":0,"turn":True},"player2":{"username":users[games[gotgameid]["player2"]]["username"],"score":0,"turn":False},"gameplay":{"1":[1,1,1,1,1],"2":[1,1,1,1],"3":[1,1,1]}}
+
+
+        emit("loadinggame",{"game":games[gotgameid]["game"],"opponent":games[gotgameid]["game"]["player2"]["username"]}  ,room=games[gotgameid]["player1"])
+        emit("loadinggame",{"game":games[gotgameid]["game"],"opponent":games[gotgameid]["game"]["player1"]["username"]}  ,room=games[gotgameid]["player2"])
+
+
+
+
+        time.sleep(1)
+        # emit("startgame",{},room=games[gotgameid]["player1"])
+        # emit("startgame",{},room=games[gotgameid]["player2"])
+
+    else:
+        emit("notfree",games[gotgameid]["creator"])
+
+
+
+
 
 
 
